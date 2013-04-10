35609659212d0eb8c528d8157949b9477422ac8c
EmilDesktop <emilovicmail@gmail.com>
2013-04-10 22:40:25 +0200

    created some initial files

---
+++ AIsOfCatan/AIsOfCatan/Agent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIsOfCatan
+{
+    interface Agent
+    {
+        void PlaceStart(GameState state); // Only (one) build settlement and build road (legally) will
+                                          // be accepted.
+
+        void PerformTurn(GameState state); // Use the GameState to perform the turn. Only legal actions
+                                           // will be accepted. Return to finish turn.
+        void HandleTrade(Trade offer); // Change the status of the Trade and return. In case of counter-
+                                       // offer remove extra Resources from the trade Lists.
+    }
+}
---
+++ AIsOfCatan/AIsOfCatan/Trade.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIsOfCatan
+{
+    public enum Resources {Brick, Lumber, Wool, Grain, Ore}; // names from wikipedia
+    public enum TradeStatus {Declined, Accepted, Countered};
+
+    class Trade
+    {
+        private TradeStatus status = TradeStatus.Declined;
+
+        // For a Wildcard put several Resources in the same inner list.
+        public List<List<Resources>> Give { get; private set; }
+        public List<List<Resources>> Want { get; private set; }
+
+        public Trade(List<List<Resources>> give, List<List<Resources>> want){
+            this.Give = give;
+            this.Want = want;
+        }
+
+        public Trade Reverse()
+        {
+            return new Trade(Want,Give);
+        }
+
+        public void Accept()
+        {
+            status = TradeStatus.Accepted;
+        }
+
+        public void CounterOffer()
+        {
+            status = TradeStatus.Countered;
+        }
+
+        public TradeStatus GetStatus()
+        {
+            return status;
+        }
+    }
+}
