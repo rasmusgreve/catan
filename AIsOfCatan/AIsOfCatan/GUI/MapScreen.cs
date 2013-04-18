d934e48ba87b485c04c892b81d5f421ffb441942
Oragada <Oragada@gmail.com>
2013-04-18 23:26:04 +0200

    GUI:
    Minor:
    * GUIRobber now no longer in incorrect namespace

--- AIsOfCatan/AIsOfCatan/GUI/MapScreen.cs
+++ AIsOfCatan/AIsOfCatan/GUI/MapScreen.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using AIsOfCatan.GUI;
 using MS.Internal.Xml.XPath;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
