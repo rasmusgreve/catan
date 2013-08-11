20a790d751ac726a3d3d085e130dc9d72204df83
EmilDesktop <emilovicmail@gmail.com>
2013-08-11 20:30:01 +0200

    Issue #26 resolved

--- AIsOfCatan/AIsOfCatan/Log/PlayRoadBuildingLogEvent.cs
+++ AIsOfCatan/AIsOfCatan/Log/PlayRoadBuildingLogEvent.cs
@@ -10,7 +10,7 @@ namespace AIsOfCatan.Log
         public int Player { get; private set; }
         private Tuple<int, int> first, second;
         public Tuple<int, int> First { get { return new Tuple<int, int>(first.Item1, first.Item2); } }
-        public Tuple<int, int> Second { get { return new Tuple<int, int>(second.Item1, second.Item2); } }
+        public Tuple<int, int> Second { get { return new Tuple<int, int>(second.Item1, second.Item2); } } // can be null
 
         public PlayRoadBuildingLogEvent(int player, Tuple<int, int> first, Tuple<int, int> second)
         {
@@ -21,7 +21,9 @@ namespace AIsOfCatan.Log
 
         public override string ToString()
         {
-            return "Player " + Player + " plays Road Building and builds on [" + First.Item1 + ", " + First.Item2 + "] and [" + Second.Item1 + ", " + Second.Item2 + "]";
+            return "Player " + Player + " plays Road Building and builds on [" + First.Item1 + ", " + First.Item2 + 
+                Second == null ? "]" 
+                : "] and [" + Second.Item1 + ", " + Second.Item2 + "]";
         }
     }
 }
